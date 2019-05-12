5487ba70c786dda62e3eb54ff624a19be3050d1e
Joshua Bretag <joshuabretag@Joshuas-MBP-793.lan>
2019-05-13 05:27:06 +1000

    Exercise 27 / 28

---
+++ ex28.py
@@ -0,0 +1,23 @@
+print("""
+
+1. True and True : True
+2. False and True : False
+3. 1 == 1 and 2 ==1 : False
+4. test == test : True
+5. 1 ==1 or 2 != 1 :
+6. True and 1 == 1 :
+7. False and 0 ! = 0 :
+8. True or 1 == 1 :
+9. "test" == "testing" : False
+10. 1 != 0 and 2 == 1 : True
+11. "test" != "testing" :
+12. "test" = 1  ;
+13. not (True and False) :
+14. not (1 == 1 and 0 != 1) :
+15. not (10 == 1 or 3 == 4) :
+16. not (1 ! = 10 or 3 == 4) :
+17. not ("testing" == "testing" and "Zed" == "Cool Guy") :
+18. 1 ==1 and not ("testing == 1 or 1 == 0") :
+19. "chunky" == "bacon" and not (3 == 4 or 3 == 3) :
+20. 3 == 3 and not ("testing" == "testing or "Python" == "Fun") :
+""")
