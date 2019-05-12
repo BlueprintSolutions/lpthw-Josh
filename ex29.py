b69932a71f9c2f8163f9cbc46781fb003b536ac9
Joshua Bretag <joshuabretag@Joshuas-MBP-793.lan>
2019-05-13 08:07:16 +1000

    Updates to exercise 29

--- ex29.py
+++ ex29.py
@@ -31,7 +31,6 @@ if people != dogs:
     print("this was a test argument")
 
 
-
 print("""
 Q: What does the if statement do?
 A: The if statement looks at the formula above it to see if the outcome is true.
@@ -45,7 +44,7 @@ Q: Why does the code under the if need to be indented four spaces?
 A: Requirement of python so it knows that arguement is related to the statement above it.
 
 Q: What happens if it isn't indented?
-A: You get an error in the terminal to indent the line of code
+A: You get an error in the terminal to indent the line of code. This is called a branch 
 
 Q: What happens if you change the inital variables for people, cats and dogs?
 A: Then you will get different True and False outcomes pending on what you change the variables to.
