ad6f92a7de50bc79110e01314b513e994b1eef7b
Joshua Bretag <joshuabretag@Joshuas-MBP-793.lan>
2019-05-15 06:07:56 +1000

    Exercise 34

---
+++ ex34.py
@@ -0,0 +1,56 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print(f"This is my list {animals}")
+
+at1 = animals[1]
+print(f"The animal at 1 is the second animal and is a {at1}")
+
+third = animals[2]
+print(f"The third animal at 2 is the third animal in the list and is a {third}")
+
+first = animals[0]
+print(f"The first animal at 0 is the first animal in the list and is a {first}")
+
+at3 = animals[3]
+print(f"The animal at 3 is the fourth animal in the list and is a {at3}")
+
+fifth = animals[4]
+print(f"The fifth animal at 4 is the fifth animal in the list and is a {fifth}")
+
+at2 = animals[2]
+print(f"The animal at 2 is the third animal in the list and is a {at2}")
+
+sixth = animals[5]
+print(f"The animal at 5 is the sixth animal in the list and is a {sixth}")
+
+at4 = animals[4]
+print(f"The animal at 4 is the fifth animal in the list and is a {at4}")
+
+
+print("""
+Q: With what you know of the difference between these types of numbers, can you explain why the year 2010 in "January 1, 2010," really is 2010 and not 2009.
+A:
+
+Q: Write some more lists and work out similar indexes until you can translate them.
+A: See below
+""")
+
+print("This is my list and indexes")
+
+toys = ['truck', 'car', 'boat', 'lego', 'barbie', 'board games', 'remote control cars']
+
+print(f"This is my list {toys}")
+
+first = toys[0]
+print(f"The first toy in my list at 0 is {first}")
+
+last = toys[6]
+print(f"The last toy in my list at 6 is {last}")
+
+at4 = toys[4]
+print(f"The fifth toy in my list at 4 is {at4}" )
+
+print("""
+Q: Use python to check your answers to this as well
+A: See above
+""")
